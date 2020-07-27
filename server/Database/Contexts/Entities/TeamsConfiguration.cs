8e63c3d93d334f914deb1c9ff09494e686d47ed3
Aaron Passchier <aaron.passchier@bissell.com>
2020-07-26 23:00:25 -0400

    Added context and configuration

---
+++ server/Database/Contexts/Entities/TeamsConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Database.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Database.Contexts.Entities
+{
+    public class TeamsConfiguration : IEntityTypeConfiguration<Team>
+    {
+        public void Configure(EntityTypeBuilder<Team> builder)
+        {
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Id).HasColumnName("ID").IsRequired();
+
+            builder.Property(t => t.Name).HasMaxLength(255).IsRequired();
+
+            builder.Property(t => t.ShortName).HasMaxLength(255);
+
+            builder.Property(t => t.Abbreviation).HasMaxLength(3).IsRequired();
+
+            builder.Property(t => t.AttackStrength).HasDefaultValue(600).IsRequired();
+
+            builder.Property(t => t.DefenseStrength).HasDefaultValue(600).IsRequired();
+
+            builder.Property(t => t.GoalieStrength).HasDefaultValue(600).IsRequired();
+
+            builder.Property(t => t.MaxPace).HasDefaultValue(40).IsRequired();
+
+            builder.Property(t => t.ShotOnGoalRate).HasDefaultValue(0.4).IsRequired();
+
+            builder.Property(t => t.PotentialNegativeShift).HasDefaultValue(10).IsRequired();
+
+            builder.Property(t => t.PotentialNegativeChance).HasDefaultValue(0.1).IsRequired();
+
+            builder.Property(t => t.PotentialPositiveShift).HasDefaultValue(10).IsRequired();
+
+            builder.Property(t => t.PotentialPositiveChance).HasDefaultValue(0.1).IsRequired();
+        }
+    }
+}
---
+++ server/Database/Contexts/SoccerSimContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database.Contexts
+{
+    public class SoccerSimContext : DbContext
+    {
+        public SoccerSimContext()
+        {
+        }
+
+        public SoccerSimContext(DbContextOptions options) : base(options){}
+
+        public DbSet<Team> Teams { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured) return;
+            optionsBuilder.UseSqlServer(
+                "Server=localhost,20240;Database=soccer-sim;User Id=sa;Password=Your_password123");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
