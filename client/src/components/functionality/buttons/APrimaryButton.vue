91510457e21e57f36a10b3f642ea065e69f3d5b3
Aaron Passchier <ap007@aquinas.edu>
2020-07-27 20:54:04 -0400

    Added APrimaryButton.vue

---
+++ client/src/components/functionality/buttons/APrimaryButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="flex flex-row justify-center px-2 py-2 mx-2 bg-blue-200" :class="ButtonClass" @click="emitClick">Continents</button>
+</template>
+
+<script lang="ts">
+import {Vue, Component, Emit} from 'vue-property-decorator';
+
+@Component
+export default class APrimaryButton extends Vue {
+  get ButtonClass(): object {
+    return {
+      'hover:shadow-md hover:underline active:bg-blue-300': true,
+    }
+  }
+
+  @Emit('click')
+  emitClick() {}
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
