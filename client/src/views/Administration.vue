65081fa046ac89a9a9f0a6c957f42d2d8b56f8e1
Aaron Passchier <ap007@aquinas.edu>
2020-07-27 20:54:15 -0400

    Implemented a primary button components

--- client/src/views/Administration.vue
+++ client/src/views/Administration.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="administration-page" class="px-4 py-2 bg-gray-300 flex flex-col flex-1">
     <div class="flex flex-row justify-between">
-      <button class="flex flex-row justify-center px-2 py-2 mx-2 bg-blue-200 flex-1">Continents</button>
-      <button class="flex flex-row justify-center px-2 py-2 mx-2 bg-blue-200 flex-1">Countries</button>
-      <button class="flex flex-row justify-center px-2 py-2 mx-2 bg-blue-200 flex-1">Divisions</button>
-      <button class="flex flex-row justify-center px-2 py-2 mx-2 bg-blue-200 flex-1">Leagues</button>
-      <button class="flex flex-row justify-center px-2 py-2 mx-2 bg-blue-200 flex-1">Divisions</button>
+      <APrimaryButton class="flex-1">Continents</APrimaryButton>
+      <APrimaryButton class="flex-1">Countries</APrimaryButton>
+      <APrimaryButton class="flex-1">Divisions</APrimaryButton>
+      <APrimaryButton class="flex-1">Leagues</APrimaryButton>
+      <APrimaryButton class="flex-1">Divisions</APrimaryButton>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import {Vue, Component} from 'vue-property-decorator';
+import APrimaryButton from "@/components/functionality/buttons/APrimaryButton.vue";
 
 @Component({
-  name: 'Administration'
+  name: 'Administration',
+  components: {APrimaryButton}
 })
 export default class Administration extends Vue {
 }
