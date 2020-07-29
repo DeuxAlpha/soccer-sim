993cbbfeb2125d0dfd61c5bedc41d12a852cf56b
Aaron Passchier <ap007@aquinas.edu>
2020-07-29 12:56:26 -0400

    Added divisions controller

---
+++ server/API/Controllers/DivisionsController.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using API.Dtos;
+using Database.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Querying.Query.Models;
+using Querying.Query.Services;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DivisionsController : ControllerBase
+    {
+        private readonly SoccerSimContext _context;
+
+        public DivisionsController(SoccerSimContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetDivisions(QueryRequest queryRequest)
+        {
+            return Ok(QueryService.GetQueryResponse(_context.Divisions.Select(d => new DivisionDto(d)), queryRequest));
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetDivision(string name)
+        {
+            return Ok(_context.Divisions.Where(d => d.Name == name).Select(d => new DivisionDto(d)));
+        }
+
+        [HttpGet("seasons/{season}")]
+        public IActionResult GetDivisions(string season)
+        {
+            return Ok(_context.Divisions.Where(d => d.Season == season).Select(d => new DivisionDto(d)));
+        }
+
+        [HttpGet("{name}/{season}")]
+        public async Task<IActionResult> GetDivision(string name, string season)
+        {
+            var division = await _context.Divisions.FirstOrDefaultAsync(d => d.Name == name && d.Season == season);
+            if (division == null) return NotFound(new {Name = name, Season = season});
+            return Ok(new DivisionDto(division));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDivision(DivisionDto divisionDto)
+        {
+            var newDivision = await _context.Divisions.AddAsync(divisionDto.Map());
+            await _context.SaveChangesAsync();
+            var returnObject = new {name = newDivision.Entity.Name, season = newDivision.Entity.Season};
+            return Created(Url.Action("GetDivision", "Divisions", returnObject), new DivisionDto(newDivision.Entity));
+        }
+
+        [HttpDelete("{name}/{season}")]
+        public async Task<IActionResult> DeleteDivision(string name, string season)
+        {
+            var division = await _context.Divisions.FirstOrDefaultAsync(d => d.Name == name && d.Season == season);
+            if (division == null) return NotFound(new {Name = name, Season = season});
+            _context.Divisions.Remove(division);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ server/API/Dtos/DivisionDto.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+using Database.Models;
+
+namespace API.Dtos
+{
+    public class DivisionDto
+    {
+        public string Name { get; set; }
+        public string Season { get; set; }
+        public string Abbreviation { get; set; }
+        public int Level { get; set; }
+        public string CountryName { get; set; }
+
+        public DivisionDto()
+        {
+
+        }
+
+        public DivisionDto(Division division)
+        {
+            Name = division.Name;
+            Season = division.Season;
+            Abbreviation = division.Abbreviation;
+            Level = division.Level;
+            CountryName = division.CountryName;
+        }
+
+        public Division Map()
+        {
+            return new Division
+            {
+                Name = Name,
+                Season = Season,
+                Abbreviation = Abbreviation,
+                Level = Level,
+                CountryName = CountryName
+            };
+        }
+    }
+}
