cf532d915ec8f22b4efd8ec5538322c17aa2d632
Aaron Passchier <ap007@aquinas.edu>
2020-07-29 12:36:10 -0400

    Added CountryDto.cs

--- server/API/Dtos/CountryDto.cs
+++ server/API/Dtos/CountryDto.cs
@@ -16,7 +16,6 @@ namespace API.Dtos
         public double PotentialNegativeChance { get; set; }
         public int MaxPace { get; set; }
         public double ShotOnGoalRate { get; set; }
-        public double CountryStrengthModifier { get; set; }
         public string ContinentName { get; set; }
         public string Image { get; set; }
 
@@ -26,13 +25,40 @@ namespace API.Dtos
 
         public CountryDto(Country country)
         {
+            Season = country.Season;
+            Name = country.Name;
+            Abbreviation = country.Abbreviation;
+            AttackStrength = country.AttackStrength;
+            DefenseStrength = country.DefenseStrength;
+            GoalieStrength = country.GoalieStrength;
+            PotentialPositiveShift = country.PotentialPositiveShift;
+            PotentialPositiveChance = country.PotentialPositiveChance;
+            PotentialNegativeShift = country.PotentialNegativeShift;
+            PotentialNegativeChance = country.PotentialNegativeChance;
+            MaxPace = country.MaxPace;
+            ShotOnGoalRate = country.ShotOnGoalRate;
+            ContinentName = country.ContinentName;
+            Image = country.Image;
         }
 
         public Country Map()
         {
             return new Country
             {
-
+                Season = Season,
+                Name = Name,
+                Abbreviation = Abbreviation,
+                AttackStrength = AttackStrength,
+                DefenseStrength = DefenseStrength,
+                GoalieStrength = GoalieStrength,
+                PotentialPositiveShift = PotentialPositiveShift,
+                PotentialPositiveChance = PotentialPositiveChance,
+                PotentialNegativeShift = PotentialNegativeShift,
+                PotentialNegativeChance = PotentialNegativeChance,
+                MaxPace = MaxPace,
+                ShotOnGoalRate = ShotOnGoalRate,
+                ContinentName = ContinentName,
+                Image = Image
             };
         }
     }
