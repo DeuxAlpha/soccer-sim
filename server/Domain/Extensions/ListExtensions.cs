0acbca8260c4e191e3497349defddd9b9dc2fe4e
Aaron Passchier <ap007@aquinas.edu>
2020-07-30 08:03:55 -0400

    Fixed pop random method in ListExtensions.cs

--- server/Domain/Extensions/ListExtensions.cs
+++ server/Domain/Extensions/ListExtensions.cs
@@ -7,7 +7,7 @@ namespace Domain.Extensions
     {
         public static T PopRandom<T>(this List<T> list)
         {
-            var randomIndex = RandomService.GetRandomNumber(0, list.Count);
+            var randomIndex = RandomService.GetRandomNumber(0, list.Count - 1);
             var randomItem = list[randomIndex];
             list.Remove(randomItem);
             return randomItem;
