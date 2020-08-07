bb251a89a85d2e26226ea0883e37de4c34710f26
Aaron Passchier <ap007@aquinas.edu>
2020-08-07 18:25:37 -0400

    Added comp rule enum

---
+++ server/Database/Enums/ComparisonRule.cs
@@ -0,0 +1,10 @@
+﻿namespace Database.Enums
+{
+    public enum ComparisonRule
+    {
+        /// <summary>Goal difference first.</summary>
+        Default,
+        /// <summary>Direct comparison between teams first.</summary>
+        Direct
+    }
+}
