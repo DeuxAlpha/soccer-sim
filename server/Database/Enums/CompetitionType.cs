2a4341babe188067d132f7f18d597c7623d18f64
Aaron Passchier <ap007@aquinas.edu>
2020-08-07 18:24:43 -0400

    Improved competition type enum

--- server/Database/Enums/CompetitionType.cs
+++ server/Database/Enums/CompetitionType.cs
@@ -2,7 +2,17 @@
 {
     public enum CompetitionType
     {
-        League,
-        Cup
+        /// <summary>E.g. To decide who gets promoted/relegated.</summary>
+        PlayOff,
+        /// <summary>Cup played within a country. E.g. DFB-Pokal</summary>
+        NationalCup,
+        /// <summary>Cup played within a continent. E.g. Champions League</summary>
+        ContinentalCup,
+        /// <summary>Cup played on a world level. E.g. Club World Cup</summary>
+        IntercontinentalCup,
+        /// <summary>Cup played by national teams within a continent. E.g. Euro</summary>
+        InternationalContinentalCup,
+        /// <summary>Cup played by national teams on a world level. E.g. World Cup</summary>
+        InternationalWorldCup
     }
 }
