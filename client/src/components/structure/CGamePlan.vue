0d2fe4d32738ae969d8c9e34bbfe8d880b599548
Aaron Passchier <ap007@aquinas.edu>
2020-08-09 10:51:19 -0400

    Added positions to fixtures

--- client/src/components/structure/CGamePlan.vue
+++ client/src/components/structure/CGamePlan.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="border border-gray-200 p-2">
     <div class="bg-gray-300 flex items-center justify-between my-1" v-for="game of games" :key="game.homeTeamName + game.awayTeamName">
-      <div class="w-5/12 text-right">{{game.homeTeamName}}</div>
+      <div class="w-5/12 text-right">
+        <span class="text-gray-500 mr-2">({{getPosition(game.homeTeamName)}})</span>
+        <span>{{game.homeTeamName}}</span>
+      </div>
       <div class="w-2/12 flex flex-col">
         <div class="flex flex-row justify-center">
           <span>{{game.homeGoals}}</span>
@@ -14,7 +17,10 @@
           <span>{{game.awayHalfGoals}})</span>
         </div>
       </div>
-      <div class="w-5/12">{{game.awayTeamName}}</div>
+      <div class="w-5/12">
+        <span>{{game.awayTeamName}}</span>
+        <span class="text-gray-500 ml-2">({{getPosition(game.awayTeamName)}})</span>
+      </div>
     </div>
   </div>
 </template>
@@ -22,10 +28,16 @@
 <script lang="ts">
 import {Vue, Component, Prop} from 'vue-property-decorator';
 import {LeagueGame} from "@/models/LeagueGame";
+import {LeagueTable} from "@/models/LeagueTable";
 
 @Component
 export default class CGamePlan extends Vue {
   @Prop({type: Array, required: true}) readonly games!: LeagueGame[];
+  @Prop({type: Object, required: true}) readonly table!: LeagueTable;
+
+  getPosition(homeTeamName: string): number {
+    return this.table.previousPositions.find(p => p.teamName === homeTeamName)!.position;
+  }
 }
 </script>
 
