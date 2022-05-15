00facf42152dcb269b7cee0a09311d98c840863e
Aaron Parsons <aaron.parsons@bissell.com>
2022-05-15 12:57:54 -0400

    Added project for seeding football data to the database

---
+++ server/Seed/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+// Seeds your database with data from football-data.co.uk, located in the ./Seeds folder and its subdirectories
