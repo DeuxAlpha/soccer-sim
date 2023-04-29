9950b10b598b8815c1fdf20075b20297483f3708
Aaron Parsons <Aaron.Parsons@bissell.com>
2023-04-29 17:56:23 -0400

    Added content to continent editor

--- client_v2/src/components/content/ContinentEditor.vue
+++ client_v2/src/components/content/ContinentEditor.vue
@@ -1,16 +1,30 @@
 <template>
-
+  <form @submit.prevent="onContinentSubmitted" class="flex flex-col">
+    <label for="continentName">Continent Name</label>
+    <input type="text" id="continentName" v-model="continentName">
+    <label for="continentSeason">Continent Season</label>
+    <input type="text" id="continentSeason" v-model="continentSeason">
+    <button type="submit">Submit</button>
+  </form>
 </template>
 
-<script lang="ts">
-import {defineComponent} from "vue";
+<script lang="ts" setup>
+import {ref} from "vue";
+import axios from 'axios';
 
-export default defineComponent({
-  name: "ContinentEditor",
-  async setup(props, context) {
+const continentName = ref("");
+const continentSeason = ref("");
 
-  }
-});
+const onContinentSubmitted = () => {
+  axios.post("https://localhost:5001/continents", {
+    name: continentName.value,
+    season: continentSeason.value
+  }).then(response => {
+    console.log(response);
+  }).catch(error => {
+    console.log(error);
+  });
+}
 </script>
 
 <style scoped lang="scss">
