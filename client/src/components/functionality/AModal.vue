4bb6e0683f2fd100baa0dc48f9156600b3a2ca33
Aaron Parsons <Aaron.Parsons@bissell.com>
2023-05-06 18:15:26 -0400

    Added modal

---
+++ client/src/components/functionality/AModal.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+
+@Component({})
+export default class AModal extends Vue {
+}
+</script>
+
+<template>
+<div class="absolute inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center">
+  <div class="bg-white rounded-lg shadow-lg p-4">
+    <slot></slot>
+  </div>
+</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
