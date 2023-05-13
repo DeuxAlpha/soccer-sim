ca2efcbdc024557a992855ae2c152d041f956a1e
Aaron Parsons <Aaron.Parsons@bissell.com>
2023-05-13 19:33:51 -0400

    Reformatted

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,24 +1,24 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import VueRouter, {RouteConfig} from 'vue-router'
 import Home from '@/views/Home.vue'
 import GameAdministration from "@/views/GameAdministration.vue";
 import LeagueImport from "@/views/LeagueImport.vue";
 
 Vue.use(VueRouter)
 
-  const routes: Array<RouteConfig> = [{
-    path: '/',
-    name: 'Home',
-    component: Home
-  },{
-    path: '/game/admin',
-    name: 'GameAdministration',
-    component: GameAdministration
-  },{
-    path: '/game/admin/import',
-    name: 'LeagueImport',
-    component: LeagueImport
-  }
+const routes: Array<RouteConfig> = [{
+  path: '/',
+  name: 'Home',
+  component: Home
+}, {
+  path: '/game/admin',
+  name: 'GameAdministration',
+  component: GameAdministration
+}, {
+  path: '/game/admin/import',
+  name: 'LeagueImport',
+  component: LeagueImport
+}
 ]
 
 const router = new VueRouter({
